9847d9c252164800cc9d62dc8f5cc70989ae4b71
Sreeram3927 <sreeram292004@gmail.com>
2024-01-03 19:51:23 +0530

    create appbar

--- app/components/app_bar.tsx
+++ app/components/app_bar.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import styles from './app_bar.module.css';
 
 const AppBar: React.FC = () => {
     return (
-        <div className="app-bar">
-            <h1>My App</h1>
+        <div className={styles.AppBar}>
+            <div>
+                <h1>My App</h1>
+            </div>
+            <NavButtons />
+            
         </div>
     );
 };
+
+function NavButtons() {
+    return (
+        <div className={styles.NavRow}>
+            <button className={styles.NavButton}>Button 1</button>
+            <button className={styles.NavButton}>Button 2</button>
+        </div>
+    )
+}
 
 export default AppBar;
