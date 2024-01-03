03f87cc71a79d91aa00924e2262cd3673cc6e238
Sreeram3927 <sreeram292004@gmail.com>
2024-01-03 20:39:51 +0530

    create hero section

---
+++ app/components/home/hero_card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./hero_card.module.css";
+
+const HeroCard: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.overlay} />
+            <div className={styles.quoteLarge}>{"\"Dancers are the messengers of the gods.\""}</div>
+            <div className={styles.quoteAuthor}>- Martha Graham</div>
+        </div>
+    )
+}
+
+export default HeroCard;
