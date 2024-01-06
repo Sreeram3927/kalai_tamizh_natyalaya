3066e4bca9225945d9b996f51a858d47c34b486e
Sreeram3927 <sreeram292004@gmail.com>
2024-01-06 21:20:26 +0530

    all images to the gallery tease

--- app/components/home/gallery_tease.tsx
+++ app/components/home/gallery_tease.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./gallery_tease.module.css";
+import Image from "next/image";
 
 const GalleryTease = () => {
     return (
@@ -8,6 +9,37 @@ const GalleryTease = () => {
             <div className={styles.topPart}>
                 <div className={styles.title}>Gallery</div>
                 <button className={styles.joinButton}>See More</button>
+            </div>
+
+            <div className={styles.imageCarousel}>
+                <Image
+                    className={styles.galleryImage} 
+                    src='/gallery/1.jpg' 
+                    alt='Image 1'
+                    width={300}
+                    height={200}
+                />
+                <Image
+                    className={styles.galleryImage} 
+                    src='/gallery/2.jpg' 
+                    alt='Image 2'
+                    width={300}
+                    height={200}
+                />
+                <Image
+                    className={styles.galleryImage} 
+                    src='/gallery/3.jpg' 
+                    alt='Image 3'
+                    width={300}
+                    height={200}
+                />
+                <Image
+                    className={styles.galleryImage} 
+                    src='/gallery/4.jpg' 
+                    alt='Image 4'
+                    width={300}
+                    height={200}
+                />
             </div>
 
         </div>
