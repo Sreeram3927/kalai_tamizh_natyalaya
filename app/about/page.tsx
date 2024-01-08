215e761318583a6d93edae6b6f22e6e0b6567c2f
Sreeram3927 <sreeram292004@gmail.com>
2024-01-08 20:44:38 +0530

    create the about guru section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import AppBar from '../components/app_bar';
 import CenteredView from "../components/centered_view";
 import Footer from '../components/footer';
+import Image from 'next/image';
 import styles from './about.module.css';
 
 export default function About() {
@@ -11,6 +12,24 @@ export default function About() {
                 <AppBar />
 
                 <div className={styles.title}>About Us</div>
+
+                <div className={styles.aboutGuru}>
+                    <Image
+                        className={styles.guruImage} 
+                        src="/about/the_guru.jpg" 
+                        alt="About Us" 
+                        height={350}
+                        width={300}
+                    />
+                    <div className={styles.guruText}>
+                        The Guru., 
+                        B.Sc. in Computer Science
+                        MBA (Master of Business Administration)
+                        MD (Doctor of Medicine)
+                        B.A. in Psychology
+                        MFA in Creative Writing (Master of Fine Arts)
+                    </div>
+                </div>
 
                 <Footer />
 
