623f9eedcd17630e9f800b3a26095f655b8900b6
Sreeram3927 <sreeram292004@gmail.com>
2024-01-15 17:30:42 +0530

    fix bg in centered view

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,44 +6,42 @@ import styles from './about.module.css';
 
 export default function About() {
     return (
-        <CenteredView>
-            <div className={styles.bg}>
+        <CenteredView isHome={false}>
             
-                <AppBar />
+            <AppBar />
 
-                <div className={styles.title}>About Us</div>
+            <div className={styles.title}>About Us</div>
 
-                <div className={styles.aboutGuru}>
-                    <Image
-                        className={styles.guruImage} 
-                        src="/about/the_guru.jpg" 
-                        alt="About Us" 
-                        height={350}
-                        width={300}
-                    />
-                    <div className={styles.guruText}>
-                        The Guru., 
-                        B.Sc. in Computer Science
-                        MBA (Master of Business Administration)
-                        MD (Doctor of Medicine)
-                        B.A. in Psychology
-                        MFA in Creative Writing (Master of Fine Arts)
-                    </div>
+            <div className={styles.aboutGuru}>
+                <Image
+                    className={styles.guruImage} 
+                    src="/about/the_guru.jpg" 
+                    alt="About Us" 
+                    height={350}
+                    width={300}
+                />
+                <div className={styles.guruText}>
+                    The Guru., 
+                    B.Sc. in Computer Science
+                    MBA (Master of Business Administration)
+                    MD (Doctor of Medicine)
+                    B.A. in Psychology
+                    MFA in Creative Writing (Master of Fine Arts)
                 </div>
-
-                <div className={styles.infoText}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, massa eget tincidunt auctor, turpis velit tincidunt sapien, eu volutpat nisi metus non metus. Sed ac facilisis tortor. Quisque vitae sem eu arcu viverra vestibulum in id nulla.
-                </div>
-
-                <div className={styles.infoText}>
-                    Pellentesque eu quam ac mi condimentum tristique. Proin quis vestibulum leo. Sed vel semper est. Curabitur viverra justo nec quam elementum fringilla. Donec dapibus, augue eu varius feugiat, purus justo varius tortor, ac ultricies erat urna et justo.
-                </div>
-
-                <button className={styles.joinButton}>Join Now</button>
-
-                <Footer />
-
             </div>
+
+            <div className={styles.infoText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, massa eget tincidunt auctor, turpis velit tincidunt sapien, eu volutpat nisi metus non metus. Sed ac facilisis tortor. Quisque vitae sem eu arcu viverra vestibulum in id nulla.
+            </div>
+
+            <div className={styles.infoText}>
+                Pellentesque eu quam ac mi condimentum tristique. Proin quis vestibulum leo. Sed vel semper est. Curabitur viverra justo nec quam elementum fringilla. Donec dapibus, augue eu varius feugiat, purus justo varius tortor, ac ultricies erat urna et justo.
+            </div>
+
+            <button className={styles.joinButton}>Join Now</button>
+
+            <Footer />
+
         </CenteredView>
     )
 }
--- app/components/centered_view.tsx
+++ app/components/centered_view.tsx
@@ -3,11 +3,26 @@ import React, { ReactNode } from 'react';
 interface CenteredViewProps {
     children: ReactNode;
     maxWidth?: number;
+    isHome?: boolean;
 }
 
-const CenteredView: React.FC<CenteredViewProps> = ({ children, maxWidth = 800 }) => {
+const CenteredView: React.FC<CenteredViewProps> = ({ children, maxWidth = 800, isHome = true }) => {
+
+    const additionalStyles: React.CSSProperties = isHome ? {} : {
+        backgroundImage: 'linear-gradient(to bottom, rgba(243, 253, 167, 0.33), rgba(0, 0, 0, 0))',
+    };
+
+    const containerStyles: React.CSSProperties = {
+        maxWidth,
+        margin: '0 auto',
+        padding: '20px',
+        textAlign: 'center',
+        height: '100vh',
+        ...additionalStyles,
+    };
+
     return (
-        <div style={{ maxWidth, margin: '0 auto', padding: '20px', textAlign: 'center' , height: '100vh'}}>
+        <div style={containerStyles}>
             {children}
         </div>
     );
